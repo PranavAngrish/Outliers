3293c30232c518ffce3b7daa15f37e8f90e6d895
Pranav <pranavangrishmusic@gmail.com>
2024-07-26 13:26:38 +0530

    backend setup

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+
+
+
+
+const asyncHandler = (requestHandler) => {
+    return (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+
+// const asyncHandler = (func) => (req,res,next) => {
+//     try{
+
+//     }catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+
+//         })
+//     }
+// }
+
+export {asyncHandler}
